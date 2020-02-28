df9cf19836fa696e703740221b38f00e75b1145d
BBrewerton <benbrewerton98@gmail.com>
2020-02-28 12:53:44 +1000

    Inital JS work (arc drawing)

---
+++ js/render_background.js
@@ -0,0 +1,15 @@
+// Function to draw the stars
+function drawStar(ctx, loc_x, loc_y) {
+  ctx.arc(loc_x, loc_y, 2)
+}
+
+// Function to render the stars onto the background a specified number of times
+function renderStars() {
+  var canv = document.getElementById('background');
+
+  if(canv.getContext) {
+    var cont = canvas.getContext('2d');
+
+
+  }
+}
